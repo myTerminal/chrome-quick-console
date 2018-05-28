d0d60d6a11d45d6dd9cfc9e8b8dfdc09e4276f58
myTerminal <team.terminal@gmail.com>
2018-05-28 16:21:58 -0500

    add first implementation

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,3 +1,118 @@
-/* global document */
+/* global require chrome window document setTimeout */
 
 import '../styles/styles.less';
+
+const manifestDetails = require('../manifest.json');
+
+let commandIndex = -1;
+
+const commandLog = [];
+
+const executeCurrentCommand = function () {
+    executeCommand(document.querySelector('#input').value);
+};
+
+const executeCommand = function (command) {
+    chrome.tabs.query(
+        {
+            active: true,
+            lastFocusedWindow: true
+        },
+        function (tabs) {
+            chrome.tabs.executeScript(
+                tabs[0].id,
+                {
+                    code: command
+                },
+                function (result) {
+                    if (result && result[0]) {
+                        appendResultToLog(result[0]);
+                    } else {
+                        appendResultToLog('null');
+                    }
+                });
+        });
+};
+
+const appendCurrentCommandToLog = function () {
+    const command = document.querySelector('#input').value;
+
+    commandLog.push(command);
+    commandIndex = -1;
+
+    document.querySelector('#log').innerHTML += '<div class="command">' + command + '</div>';
+    document.querySelector('#input').value = '';
+};
+
+const appendResultToLog = function (result) {
+    document.querySelector('#log').innerHTML += '<div class="result">> ' + result + '</div>';
+    scrollLogToBottom();
+};
+
+const scrollLogToBottom = function () {
+    const logContainer = document.querySelector('#console-log');
+
+    logContainer.scrollTop = logContainer.scrollHeight;
+};
+
+const clearLog = function () {
+    document.querySelector('#log').innerHTML = '';
+};
+
+const loadPreviousCommand = function () {
+    if (!commandIndex || commandIndex === -1) {
+        commandIndex = commandLog.length - 1;
+    } else {
+        commandIndex -= 1;
+    }
+
+    loadCommandToInput(commandIndex);
+};
+
+const loadNextCommand = function () {
+    if (commandIndex === commandLog.length - 1 || commandIndex === -1) {
+        commandIndex = 0;
+    } else {
+        commandIndex += 1;
+    }
+
+    loadCommandToInput(commandIndex);
+};
+
+const loadCommandToInput = function (index) {
+    const inputBox = document.querySelector('#input');
+
+    if (commandIndex !== -1) {
+        inputBox.value = commandLog[index];
+        setTimeout(function () {
+            inputBox.selectionStart = inputBox.value.length;
+            inputBox.selectionEnd = inputBox.value.length;
+        });
+    }
+};
+
+window.addEventListener('load', function () {
+    document.querySelector('#title').innerText = manifestDetails.name + ' ' + manifestDetails.version;
+
+    document.querySelector('#clear').onclick = clearLog;
+
+    document.querySelector('#evaluate').onclick = function () {
+        executeCurrentCommand();
+        appendCurrentCommandToLog();
+    };
+
+    document.querySelector('#input').onkeydown = function (e) {
+        if (e.keyCode === 13) {
+            executeCurrentCommand();
+            appendCurrentCommandToLog();
+            return false;
+        } else if (e.keyCode === 38) {
+            loadPreviousCommand();
+        } else if (e.keyCode === 40) {
+            loadNextCommand();
+        }
+
+        return true;
+    };
+});
+
